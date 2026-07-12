338a714e7b7b8eba1404e921ab99fe973c14f241
trippykitty808 <bpmckenney@gmail.com>
2026-07-12 14:43:55 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // what tells a phone that already has the app installed there's something
 // new to fetch. Editing an app file alone does NOT trigger an update; this
 // script's own bytes have to change too.
-const CACHE_VERSION = 'little-steps-v11';
+const CACHE_VERSION = 'little-steps-v12';
 const SHELL_FILES = [
   './',
   './index.html',
@@ -19,6 +19,7 @@ const SHELL_FILES = [
   './js/data/milestones.js',
   './js/data/guideResources.js',
   './js/data/nutrition.js',
+  './js/data/recipes.js',
   './js/screens/welcome.js',
   './js/screens/setupChild.js',
   './js/screens/setupCaregiver.js',
@@ -37,6 +38,7 @@ const SHELL_FILES = [
   './js/screens/printPreview.js',
   './js/screens/planner.js',
   './js/screens/nutrition.js',
+  './js/screens/recipeDetail.js',
   './js/tabBar.js',
   './js/childSwitcher.js',
   './js/authScreen.js',
